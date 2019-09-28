106cb3a7375dd9ebd4ed674e344789a2db67751c
Hibbs, Kevin <Kevin.Hibbs@agilysys.com>
2019-09-28 10:14:07 -0700

    removed filterstaus value - it did not help

--- server.js
+++ server.js
@@ -17,28 +17,30 @@ let client = new twitter({
   access_token_key: process.env.token_key,
   access_token_secret: process.env.token_secret
 });
-let follow = { follow: `765582935831576576,146436211,22536055,7212562,5920532,34176543`, filter_level: 'medium' };
-//Medium filter level should limit tweets to just orringal (no retweets) users. 
+let follow = {
+  follow: `765582935831576576,146436211,22536055,7212562,5920532,34176543,14372091,388926026`
+};
 
 console.log('Starting Twitter follow stream');
 client.stream('statuses/filter', follow, function (stream) {
-    stream.on('data', function (tweet) {
-      if (mode === 'dev') { fs.appendFileSync(`./logs/tweetIn_${tweet.id}.json`, JSON.stringify(tweet))};
-      console.log(`I just got a tweet from ${tweet.user.name} - ${tweet.id}`);
-      //Check to see if this is the user replying to another user, or if it is a retweet. 
-      if (tweet.in_reply_to_user_id === null && tweet.in_reply_to_status_id === null && !(tweet.retweeted_status)) {
-        if (mode === 'dev') { fs.appendFileSync(`./logs/tweetOut_${tweet.id}.json`, JSON.stringify(tweet))};
-        console.log(`Try to retweet ${tweet.id}`);
-        client.post('statuses/retweet/' + tweet.id_str, function (error, reTweet, callback) {
-          if (error) {
-            console.log(`Error in client.post: ${{error}}`);
-            return;
-          };
-          console.log('!! -- I just re-tweeded ' + tweet.user.name);
-        });
+  stream.on('data', function (tweet) {
+    //Check to see if this is the user replying to another user, or if it is a retweet. 
+    if (tweet.in_reply_to_user_id === null && tweet.in_reply_to_status_id === null && !(tweet.retweeted_status)) {
+      if (mode === 'dev') {
+        fs.appendFileSync(`./logs/${tweet.id}.json`, JSON.stringify(tweet))
       };
-    });
-    stream.on('error', function (error) {
-      console.log('Stream.on says --You done got an error! ' + error);
-    });
+      console.log(`I just got a tweet from ${tweet.user.name} - ${tweet.id}`);
+      console.log(`Try to retweet ${tweet.id}`);
+      client.post('statuses/retweet/' + tweet.id_str, function (error, reTweet, callback) {
+        if (error) {
+          console.log(`Error in client.post: ${{error}}`);
+          return;
+        };
+        console.log('!! -- I just re-tweeded ' + tweet.user.name);
+      });
+    };
   });
+  stream.on('error', function (error) {
+    console.log('Stream.on says --You done got an error! ' + error);
+  });
+});
